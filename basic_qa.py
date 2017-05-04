11bc8d05c755f28948f33ddb56b7f2f9a6bd0a80
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-05-04 17:14:33 +1000

    57% answer sentences found

--- basic_qa.py
+++ basic_qa.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-
+import numpy as np
 import nltk
 import json
 
@@ -13,9 +13,9 @@ word_tokenizer = nltk.tokenize.regexp.WordPunctTokenizer()
 
 def remove_stop(sentence):
 	new = []
-	for word in sentence.split():
-		if word in STOPWORDS: continue
-		new.append(word)
+	for word in sentence:
+		if not word in STOPWORDS:
+			new.append(word)
 	return new
 
 # def get_bow(document):
@@ -34,10 +34,70 @@ def lemmatize(word):
 def lemmatize_doc(document):
 	output = []
 	for word in document:
-		output.append(lemmatize(word))
+		if word.isalpha():
+			output.append(lemmatize(word.lower()))
 	return output
 
+def get_freqencies(documents):
+	term_freqs = []
+	doc_freq = {}
+	for document in documents:
+		document = word_tokenizer.tokenize(document)
+		document = lemmatize_doc(document)
+		document = remove_stop(document)
+		term_freq = {}
+		for word in document:
+			term_freq[word] = term_freq.get(word, 0) + 1
+		for word in list(set(document)):
+			doc_freq[word] = doc_freq.get(word, 0) + 1
+		term_freqs.append(term_freq)
+	return term_freqs, doc_freq
 
+def get_tf_idf(term_freqs, doc_freq):
+	tf_idf = []
+	words = doc_freq.keys()
+	for doc_dict in term_freqs:
+		doc_vector = []
+		words_in_doc = set(doc_dict.keys())
+		for word in words:
+			if word in words_in_doc:
+				doc_vector.append(doc_dict[word] * 1.0 / doc_freq[word])
+			else: doc_vector.append(0)
+		tf_idf.append(doc_vector)
+	return words, tf_idf
+
+def get_inverted_index(words, tf_idf):
+	posting = {}
+	for i in range(len(words)):
+		posting[words[i]] = posting.get(words[i], [])
+		for doc_id in range(len(tf_idf)):
+			word_weight = tf_idf[doc_id][i]
+			if word_weight != 0:
+				posting[words[i]].append((doc_id, word_weight))
+	return posting
+
+def process_query(query):
+	return remove_stop(lemmatize_doc(word_tokenizer.tokenize(query)))
+
+def prepare_doc(doc_set):
+	term_freqs, doc_freq = get_freqencies(doc_set)
+	words, tf_idf = get_tf_idf(term_freqs, doc_freq)
+	posting = get_inverted_index(words, tf_idf)
+	return posting
+
+def eval_query(query, posting, no_docs):
+	scores = {}
+	for term in query:
+		posting_list = posting.get(term, [])
+		for (doc_id, weight) in posting_list:
+			scores[doc_id] = scores.get(doc_id, 0) + weight
+	return sorted(scores.items(), key=lambda x:x[1], reverse=True)
+
+def get_most_prob_sent(question, doc_set):
+	query = process_query(question)
+	posting = prepare_doc(doc_set)
+	no_docs = len(doc_set)
+	return eval_query(query, posting, no_docs)[:3]
 
 if __name__ == '__main__':
 	documents = []
@@ -45,31 +105,28 @@ if __name__ == '__main__':
 	BOWs = []
 	dict_vectorizer = DictVectorizer()
 
-	with open('QA_test.json') as test_file:
-		test = json.load(test_file)
+	with open('QA_dev.json') as dev_file:
+		dev = json.load(dev_file)
 
-	term_freqs = []
-	doc_freqs = []
-	for i in range(len(test)):
-		doc_freq[i] = {}
-		for document in test[i]['sentences']:
-			document = word_tokenizer.tokenize(document)
-			document = lemmatize(document)
-			document = remove_stop(document)
-			term_freq = {}
-			for word in document:
-				term_freq[word] = term_freq.get(word, 0) + 1
-			if word in document:
-				doc_freq[word] = doc_freq.get(word, 0) + 1
-		term_freqs.append(term_freq)
-		doc_freqs.append(doc_freq)
+	total = 0.0
+	match = 0.0
+	for trial in dev:
+		doc_set = trial['sentences']
+		posting = prepare_doc(doc_set)
+		no_docs = len(doc_set)
+		for question in trial['qa']:
+			query = process_query(question['question'])
+			possible_sents = eval_query(query, posting, no_docs)
+			total += 1
+			if len(possible_sents) == 0:
+				continue
+			else: sent = possible_sents[0]
+			if sent[0] == question['answer_sentence']:
+				match += 1
 
-	tf_idfs = []
-	for i in range(len(term_freqs)):
-		tf_idf = {}
-		for word, count in term_freqs[i].items():
-			tf_idf[word] = count * (1.0 / doc_freqs[i][word])
+	print match / total
 
-	
+
+
 
 
