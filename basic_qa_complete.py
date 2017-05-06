cfd83b8d21a177dbdcd7e0331a8d75f4049b0072
Jack <Jack@Jacks-MacBook-Pro.local>
2017-05-06 21:11:44 +1000

    take sentence retrieval ranking into account selecting best answer

--- basic_qa_complete.py
+++ basic_qa_complete.py
@@ -173,44 +173,89 @@ def get_question_type(question):
 	else:
 		return "OTHER"
 
-def get_score(question, answer, doc_set):
-	"""Calculate the score of an answer given a question.
+def contains_all(items, elems):
+	for e in elems:
+		if e not in items:
+			return False
+	return True
+
+# def get_score(question, answer, doc_set, sentences):
+# 	"""Calculate the score of an answer given a question.
+
+# 	Args:
+# 		question (str): the question as a string
+# 		answers [(str, str, str)]: an answer to the question
+# 			being a 3-tuple of (sentence, entity, entity type)
+# 		doc_set [str]: a list of all answers, indexed by answer id
+
+# 	Returns:
+# 		(float): score of answer
+# 	"""
+# 	# First, answers whose content words all appear
+# 	# in the question should be ranked lowest.
+# 	ans_words = word_tokenizer.tokenize(doc_set[answer[0]])
+# 	question_words = set(word_tokenizer.tokenize(question))
+# 	if contains_all(question_words, ans_words):
+# 		return 0.0
+
+# 	# Second, answers which match the question type
+# 	# should be ranked higher than those that don't;
+# 	if answer[2] != get_question_type(question):
+# 		return 1.0
+
+# 	# Third, among entities of the same type, the
+# 	# prefered entity should be the one which is closer
+# 	# in the sentence to a closed-class word from the question.
+# 	# TODO
+
+# 	rank = sentences.index(answer[0])
+# 	return 1.0 + 100.0/(rank+1)
+
+def make_answer_cmp_func(question, doc_set, sentences):
+	"""Make comparision function of answers.
 
 	Args:
 		question (str): the question as a string
-		answers [(str, str, str)]: an answer to the question
-			being a 3-tuple of (sentence, entity, entity type)
 		doc_set [str]: a list of all answers, indexed by answer id
 
 	Returns:
-		(float): score of answer
+		comparision funtion of two answers, more relavant answer
+		is considered greater
 	"""
-	# First, answers whose content words all appear
-	# in the question should be ranked lowest.
-	ans_words = word_tokenizer.tokenize(doc_set[answer[0]])
 	question_words = set(word_tokenizer.tokenize(question))
-	all_appear = True
-	for w in ans_words:
-		if w not in question_words:
-			all_appear = False
-			break
-	if all_appear:
-		return 0.0
+	def cmp_answer(a, b):
+		# First, answers whose content words all appear
+		# in the question should be ranked lowest.
+		a_words = word_tokenizer.tokenize(doc_set[a[0]])
+		b_words = word_tokenizer.tokenize(doc_set[b[0]])
+		a_all_appear = contains_all(question_words, a_words)
+		b_all_appear = contains_all(question_words, b_words)
+		if a_all_appear != b_all_appear:
+			return b_all_appear - a_all_appear
 
-	# Second, answers which match the question type
-	# should be ranked higher than those that don't;
-	if answer[2] != get_question_type(question):
-		return 1.0
+		# Second, answers which match the question type
+		# should be ranked higher than those that don't;
+		question_type = get_question_type(question)
+		a_matches_type = a[2] == question_type
+		b_matches_type = b[2] == question_type
+		if a_matches_type != b_matches_type:
+			return a_matches_type - b_matches_type
 
-	# Third, among entities of the same type, the
-	# prefered entity should be the one which is closer
-	# in the sentence to a closed-class word from the question.
-	# TODO
+		# Third, among entities of the same type, the
+		# prefered entity should be the one which is closer
+		# in the sentence to a closed-class word from the question.
+		# TODO
 
-	return 3.0
+		# # consider relavance in sentence retrieval
+		return sentences.index(a[0]) - sentences.index(b[0])
+		# return 0
+	return cmp_answer
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-def get_best_answer(question, answers, doc_set):
+from functools import cmp_to_key
+def get_best_answer(question, answers, doc_set, sentences):
 	"""Return the best answer from answers to a question.
 
 	Args:
@@ -218,14 +263,18 @@ def get_best_answer(question, answers, doc_set):
 		answers [(str, str, str)]: a list of answers,
 			each being a 3-tuple of (sentence, entity, entity type)
 		doc_set [str]: a list of all answers, indexed by answer id
+		sentences [int]: a list of sentence id's sorted by relavance
 
 	Returns:
 		(str, str, str): the best answer to the question
 	"""
-	answer_scores = []
-	for ans in answers:
-		answer_scores.append((ans, get_score(question, ans, doc_set)))
-	return max(answer_scores, key=lambda x: x[1])[0]
+	# answer_scores = []
+	# for ans in answers:
+	# 	answer_scores.append((ans, get_score(question, ans, doc_set)))
+	# return max(answer_scores, key=lambda x: x[1])[0]
+	key_func = cmp_to_key(make_answer_cmp_func(question, doc_set, sentences))
+	pp.pprint(sorted(answers, reverse=True, key=key_func))
+	return max(answers, key=key_func)
 
 
 
@@ -305,17 +354,30 @@ def test_with_dev():
 				continue
 			# search for entities in possible sents
 			matches = []
+
+			# # take only the best match in sentence retrieval
+			# matches = [e for e in entities if e[0] == possible_sents[0]]
+
+			# OR...
+
+			# take all sentences into ranking
 			for sent in possible_sents:
 				matches.extend([e for e in entities if e[0] == sent])
-			
+
+			# # determine if correct answer exists in entities
+			# matches_entities = {m[1] for m in matches}
+			# if question['answer'] in matches_entities:
+			# 	match += 1
+
 			if len(matches) == 0:
 				continue
 			
 			# find best answer
-			best_match = get_best_answer(question['question'], matches, doc_set)
+			best_match = get_best_answer(question['question'], matches, doc_set, possible_sents)
 
-			print "expected: ", question['answer']
-			print "actual: ", best_match[1]
+			print "question:", question['question']
+			print "expected:", question['answer']
+			print "actual:", best_match[1]
 			print "\n\n"
 
 			if best_match[1] == question['answer']:
