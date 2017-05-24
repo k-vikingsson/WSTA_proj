02f1ecd318305599e2ec736025fb5075d3f833ba
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-05-24 21:48:20 +1000

    nothing but basics

---
+++ ner_test04.py
@@ -0,0 +1,177 @@
+import os
+import nltk
+import json
+from nltk.tag import StanfordNERTagger
+
+# nltk.download('maxent_treebank_pos_tagger')
+# nltk.download('averaged_perceptron_tagger')
+
+os.environ['CLASSPATH'] = '/usr/share/stanford-ner/stanford-ner.jar'
+os.environ['STANFORD_MODELS'] = '/usr/share/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz'
+
+
+classifier = os.environ.get('STANFORD_MODELS')
+jar = os.environ.get('CLASSPATH')
+ 
+st = StanfordNERTagger(classifier,jar)
+word_tokenizer = nltk.tokenize.regexp.WordPunctTokenizer()
+lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
+
+
+#sents = ["Rami Eid is studying at Stony Brook University in NY","Anna Brown like Beijing 0709 0303 Shanghai"]
+
+def process_doc_ner(doc_set):
+    # doc as a single sentence
+    new_docs = []
+    for doc in doc_set:
+        doc = word_tokenizer.tokenize(doc)
+        new_docs.append(doc)
+    return new_docs 
+
+def get_next_tag (tagged_sent,cur_tag):
+    for token, tag, i in tagged_sent:
+        if tag != cur_tag:
+            pos = tagged_sent.index((token,tag,i))
+            return pos
+
+def get_continuous_chunks(tagged_sents):
+    sents_chunks = []
+    for tagged_sent in tagged_sents:
+        tagged_sent.append(('end','END','END'))
+        continuous_chunk = []
+        sent_not_empty = True
+
+
+        while sent_not_empty:
+            cur_tag = tagged_sent[0][1]
+            pos = get_next_tag(tagged_sent,cur_tag)
+            chunk = tagged_sent[0:pos]
+            continuous_chunk.append(chunk)
+            tagged_sent = tagged_sent[pos:]
+
+            if len(tagged_sent) == 1:
+                sent_not_empty = False
+
+        sents_chunks.append(continuous_chunk)
+    return sents_chunks
+
+def get_tagged(processed_docs,no_docs):
+    ner_tagged_sents = st.tag_sents(processed_docs)
+    pos_tagged_sents = nltk.pos_tag_sents(processed_docs)
+    tagged_sents = []
+
+    for j in range (0,no_docs):
+        tagged_sent =[]
+        no_tokens = len(ner_tagged_sents[j])
+
+        for i in range (0,no_tokens):
+            token = ner_tagged_sents[j][i][0]
+            tag = ner_tagged_sents[j][i][1]
+
+
+            if token != '':
+                if tag != 'O':
+                    tagged_sent.append((token,tag,i))
+
+                else:
+                    if i != 0 and token[0].isupper():
+                        tag = 'OTHER'
+                        tagged_sent.append((token,tag,i))
+
+                    elif pos_tagged_sents[j][i][1] == 'CD':
+                        # 5/five people / meters
+                        if no_tokens > i+1:
+                            if pos_tagged_sents[j][i+1][1] == 'NN' or pos_tagged_sents[j][i+1][1] == 'NNS':
+                                token = token + ' ' + pos_tagged_sents[j][i+1][0]
+
+                            # 14,372
+                            elif (pos_tagged_sents[j][i+1][1] == ',' or pos_tagged_sents[j][i+1][1] == '.') and no_tokens > i+2 and pos_tagged_sents[j][i+2][1] == 'CD':
+                                token = token + pos_tagged_sents[j][i+1][0] + pos_tagged_sents[j][i+2][0]
+                                tem = pos_tagged_sents[j].pop(i+2)
+                                tem_token = tem[0]
+                                pos_tagged_sents[j].insert(i+2,(tem_token,'NONE'))
+
+                                # 16,290 people
+                                if no_tokens > i + 3 and (pos_tagged_sents[j][i+3][1] == 'NN' or pos_tagged_sents[j][i+3][1] == 'NNS'):
+                                    token = token + ' ' + pos_tagged_sents[j][i+3][0]
+
+
+                                # 1.5 million
+                                elif no_tokens > i + 3 and pos_tagged_sents[j][i+3][1] == 'CD':
+                                    token = token + ' ' + pos_tagged_sents[j][i+3][0]
+                                    tem1 = pos_tagged_sents[j].pop(i+3)
+                                    tem1_token = tem1[0]
+                                    pos_tagged_sents[j].insert(i+2,(tem1_token,'NONE'))
+
+                                    if no_tokens > i + 4 and (pos_tagged_sents[j][i+4][1] == 'NN' or pos_tagged_sents[j][i+4][1] == 'NNS'):
+                                        token = token + ' ' + pos_tagged_sents[j][i+4][0]
+
+
+
+
+
+                            tag = 'NUMBER'
+                            tagged_sent.append((token,tag,i))
+
+
+                            pos_tagged_sents[j][i+1][1] == ','
+
+
+                    else:
+                        tagged_sent.append((token,tag,i))
+
+            else:
+                tagged_sent.append((token,tag,i))
+
+        tagged_sents.append(tagged_sent)
+    return tagged_sents
+
+
+
+
+def parse_docs(doc_set):
+    processed_docs = process_doc_ner(doc_set)
+    no_docs = len(processed_docs)
+    tagged_sents = get_tagged(processed_docs,no_docs)
+
+    name_entity_list = get_continuous_chunks(tagged_sents)
+
+    doc_ne_pairs = []
+    for i in range (0,no_docs):
+        name_entity = name_entity_list[i]
+        # name_entity_str = [" ".join([token for token, tag in ne]) for ne in name_entity]
+        name_entity_pairs = [(i," ".join([token for token, tag, start in ne]), ne[0][1],ne[0][2],ne[-1][2]) for ne in name_entity]
+        for sent_id,entity,tag,start_i,end_i in name_entity_pairs:
+            if tag != 'O':
+                if tag == 'ORGANIZATION':
+                    doc_ne_pairs.append((sent_id,entity,'OTHER',start_i,end_i))
+                elif tag == 'NUMBER':
+                    text = word_tokenizer.tokenize(entity)
+                    n = len(text)
+                    if n != 1:
+                        end_i = end_i + n - 1
+                    doc_ne_pairs.append((sent_id,entity,tag,start_i,end_i))
+                else:
+                    doc_ne_pairs.append((sent_id,entity,tag,start_i,end_i))
+    return doc_ne_pairs
+
+if __name__ == '__main__':
+    with open('QA_dev.json') as dev_file:
+        dev = json.load(dev_file)
+
+    for i in range (0,5):
+        doc_set = dev[i]['sentences']
+        print parse_docs(doc_set)
+
+    #     for pair in test:
+    #         if pair[2] == 'NUMBER':
+    #             print pair
+    # # sents =[dev[2]['sentences'][35],dev[2]['sentences'][36],dev[2]['sentences'][47]]
+    # # parse_docs(sents)
+    # print dev[4]['sentences'][227]
+
+
+    # sent2 = word_tokenizer.tokenize('If you go to bed early, then you will wake up early')
+    # pos_sent2 = nltk.pos_tag(sent2)
+
+
---
+++ ranking.py
@@ -0,0 +1,209 @@
+import nltk
+word_tokenizer = nltk.tokenize.regexp.WordPunctTokenizer()
+
+common_measurements = set()
+with open("common_measurements.txt") as file:
+	for line in file:
+		common_measurements.add(line.strip())
+
+common_localities = set()
+with open("common_localities.txt") as file:
+	for line in file:
+		common_localities.add(line.strip())
+
+def get_question_type(question_words):
+	"""Determine question type.
+
+	Args:
+		question_words ([str]): list of words in question
+
+	Returns:
+		(str): type of question as a string
+	"""
+	# TODO more rules
+	if "who" in question_words:
+		return "PERSON"
+	elif "where" in question_words:
+		return "LOCATION"
+	elif "how" in question_words and "many" in question_words:
+		return "NUMBER"
+	elif "what" in question_words and "year" in question_words:
+		return "NUMBER"
+	elif "when" in question_words:
+		return "NUMBER"
+	else:
+		if "what" in question_words or "which" in question_words:
+			if "king" in question_words: return "PERSON"
+			elif "name" in question_words: return "PERSON"
+			for w in question_words:
+				if w in common_measurements: return "NUMBER"
+				elif w in common_localities: return "LOCATION"
+		return "OTHER"
+
+def contains_all(items, elems):
+	for e in elems:
+		if e not in items:
+			return False
+	return True
+
+# def get_score(question, answer, doc_set, sentences):
+# 	"""Calculate the score of an answer given a question.
+
+# 	Args:
+# 		question (str): the question as a string
+# 		answers [(str, str, str)]: an answer to the question
+# 			being a 3-tuple of (sentence, entity, entity type)
+# 		doc_set [str]: a list of all answers, indexed by answer id
+
+# 	Returns:
+# 		(float): score of answer
+# 	"""
+# 	# First, answers whose content words all appear
+# 	# in the question should be ranked lowest.
+# 	ans_words = word_tokenizer.tokenize(doc_set[answer[0]])
+# 	question_words = set(word_tokenizer.tokenize(question))
+# 	if contains_all(question_words, ans_words):
+# 		return 0.0
+
+# 	# Second, answers which match the question type
+# 	# should be ranked higher than those that don't;
+# 	if answer[2] != get_question_type(question):
+# 		return 1.0
+
+# 	# Third, among entities of the same type, the
+# 	# prefered entity should be the one which is closer
+# 	# in the sentence to a closed-class word from the question.
+# 	# TODO
+
+# 	rank = sentences.index(answer[0])
+# 	return 1.0 + 100.0/(rank+1)
+
+def get_closed_class_words(question_words):
+	tagged = nltk.pos_tag(question_words, tagset="universal")
+	# consider pronouns, determiners, conjunctions, and prepositions as closed class
+	return [p[0] for p in tagged if p[1] in ["PRON", "DET", "CONJ", "ADP", "AUX", "NUM", "PART"]]
+
+def get_open_class_words(question_words):
+	tagged = nltk.pos_tag(question_words, tagset="universal")
+	# consider pronouns, determiners, conjunctions, and prepositions as closed class
+	return [p[0] for p in tagged if p[1] in ["ADJ", "ADV", "INTJ", "NOUN", "PROPN", "VERB"]]
+
+def get_dist_to_question_word(target_words, sentence_words, entity):
+	# # print closed_class_words
+	# answer = entity[1].lower()
+	# answer_words = nltk.word_tokenize(answer)
+	# # cannot proceed if answer word not found in sentence
+	# for w in answer_words:
+	# 	if w not in sentence_words:
+	# 		return None
+	# get positions of closed class question words
+	question_words_pos = []
+	for w in target_words:
+		for i in range(len(sentence_words)):
+			if w == sentence_words[i]:
+				question_words_pos.append(i)
+	# print question_words_pos
+	# cannot proceed if no such closed word in sentence
+	if len(question_words_pos) == 0:
+		return None
+	# # (naive way to) find answer position in sentence
+	# answer_start_pos = sentence_words.index(answer_words[0])
+	# answer_end_pos = sentence_words.index(answer_words[-1])
+	answer_start_pos = entity[3]
+	answer_end_pos = entity[4]
+	# print "ans start", answer_start_pos
+	# print "ans end", answer_end_pos
+	# calculate distance and find closest
+	dists = [ min(abs(p-answer_start_pos), abs(p-answer_end_pos))
+		for p in question_words_pos ]
+	# print dists
+	return sum(dists)
+
+def cmp_answer(a, b):
+	# First, answers whose content words all appear
+	# in the question should be ranked lowest.
+	a_all_appear = a[-3]
+	b_all_appear = b[-3]
+	if a_all_appear != b_all_appear:
+		return b_all_appear - a_all_appear
+	# Second, answers which match the question type
+	# should be ranked higher than those that don't;
+	a_matches_type = a[-2]
+	b_matches_type = b[-2]
+	if a_matches_type != b_matches_type:
+		return a_matches_type - b_matches_type
+	# consider relavance (rank) in sentence retrieval
+	a_rank = a[-4]
+	b_rank = b[-4]
+	if a_rank != b_rank:
+		return b_rank - a_rank
+	# Third, among entities of the same type, the
+	# prefered entity should be the one which is closer
+	# in the sentence to a closed-class word from the question.
+	a_dist = a[-1]
+	b_dist = b[-1]
+	if a_dist != b_dist:
+		if a_dist == None:
+			return -1
+		elif b_dist == None:
+			return 1
+		else:
+			return b_dist - a_dist
+	return 0
+
+def add_answer_properties(question_words, question_type, open_class_words, answer, doc_set, sentences):
+	answer_words = word_tokenizer.tokenize(answer[1].lower())
+	answer_sent_words = [ w.lower() for w in word_tokenizer.tokenize(doc_set[answer[0]]) ]
+	added = list(answer)
+	added.append(sentences.index(answer[0]))
+	added.append(contains_all(question_words, answer_words))
+	added.append(answer[2] == question_type)
+	added.append(get_dist_to_question_word(open_class_words, answer_sent_words, answer))
+	return tuple(added)
+
+from functools import cmp_to_key
+def get_best_answer(question, answers, doc_set, sentences):
+	"""Return the best answer from answers to a question.
+
+	Args:
+		answers [(str, str, str)]: a list of answers,
+			each being a 3-tuple of (sentence, entity, entity type)
+		doc_set [str]: a list of all answers, indexed by answer id
+		sentences [int]: a list of sentence id's sorted by relavance
+
+	Returns:
+		(str, str, str): the best answer to the question
+	"""
+	question_words = { w.lower() for w in word_tokenizer.tokenize(question) }
+	question_type = get_question_type(question_words)
+	open_class_words = get_open_class_words(question_words)
+	answers_added = [
+		add_answer_properties(
+			question_words,
+			question_type,
+			open_class_words,
+			a,
+			doc_set,
+			sentences)
+		for a in answers
+	]
+	key_func = cmp_to_key(cmp_answer)
+	return max(answers_added, key=key_func)
+	# return get_top_answers(question, answers, doc_set, sentences)[0]
+
+def get_top_answers(question, answers, doc_set, sentences):
+	question_words = { w.lower() for w in word_tokenizer.tokenize(question) }
+	question_type = get_question_type(question_words)
+	open_class_words = get_open_class_words(question_words)
+	answers_added = [
+		add_answer_properties(
+			question_words,
+			question_type,
+			open_class_words,
+			a,
+			doc_set,
+			sentences)
+		for a in answers
+	]
+	key_func = cmp_to_key(cmp_answer)
+	return sorted(answers_added, reverse=True, key=key_func)[:20]
