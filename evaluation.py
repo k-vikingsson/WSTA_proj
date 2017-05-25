e0c3204ee6479cc1c258868b1f044184394c1f4b
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-05-25 21:53:10 +1000

    tidy up

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,10 @@
+
+
 def reciprocal_rank(answer, results):
     if answer not in results:
         return 0.0
     else:
         return 1.0 / (results.index(answer) + 1)
-
-def get_word_error_rate(answer, result):
-    pass
 
 def is_partial_match(answer, target):
     if answer == target: return False
